cbf96045c219cfb92b9cf59b94005e6ae478327d
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-06-24 15:47:52 +0700

    Display SnackBar

--- app/src/main/java/com/rujirakongsomran/kt_snackbar/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_snackbar/MainActivity.kt
@@ -2,10 +2,25 @@ package com.rujirakongsomran.kt_snackbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.rujirakongsomran.kt_snackbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Snackbar.make(
+                it,
+                "When you click on the “Show Snackbar” button, a Snackbar appears at the bottom of the screen.",
+                Snackbar.LENGTH_SHORT
+            )
+                .show()
+        }
     }
 }
